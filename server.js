1d52adc36a011a8c3eff2680297b79d790444659
Stephen Weiss <stephencweiss@gmail.com>
2018-11-27 17:04:42 -0800

    Refactored proxy for local instance of Descriptions

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 
 
 
